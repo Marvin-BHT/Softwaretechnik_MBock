1e4a95883c65db309700da705cb86ccde01e9c45
Marvin <mabo3343@bht-berlin.de>
2026-06-22 12:40:20 +0200

    11_Aufgabe-Test, Teil A2, ShoppingCart 4.Red, Methode addItem fehlt, 12:40

--- 11_Aufgabe_Test/A2/ShoppingCart_Test.java
+++ 11_Aufgabe_Test/A2/ShoppingCart_Test.java
@@ -4,12 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ShoppingCart_Test {
-
+	
 	@Test
 	void test_empty_cart_has_zero_total() {
 		ShoppingCart cart = new ShoppingCart();
 		
-		assertEquals(0.0, cart.getTotal());
+		assertEquals(0, cart.getTotal());
 	}
 
+	@Test
+	void test_add_item_increases_total() {
+		ShoppingCart cart = new ShoppingCart();
+		cart.addItem("EinItem", 3);
+		
+		assertEquals(3, cart.getTotal());
+	}
 }
