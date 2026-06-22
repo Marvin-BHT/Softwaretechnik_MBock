1c4c1bbe4cc4329ab97a7a20063bb6f36573ab7b
Marvin <mabo3343@bht-berlin.de>
2026-06-22 13:42:35 +0200

    11_Aufgabe-Test, Teil A2, ShoppingCart 8.Red, get_quantity eingebunden, neue Map für Menge hinzugefügt, AddItem angepasst, noch Red da getTotal noch angepasst werden muss 13:41

--- 11_Aufgabe_Test/A2/ShoppingCart.java
+++ 11_Aufgabe_Test/A2/ShoppingCart.java
@@ -6,6 +6,7 @@ import java.util.Map;
 public class ShoppingCart {
 
 	public Map<String, Integer> warenkorb = new HashMap<>();
+	public Map<String, Integer> warenmenge = new HashMap<>();
 	
 	public int getTotal() {
 		int gesamt = 0;
@@ -17,5 +18,15 @@ public class ShoppingCart {
 	
 	public void addItem(String name, int preis) {
 		warenkorb.put(name, preis);
+		
+		if (warenmenge.containsKey(name)) {
+			warenmenge.put(name, warenmenge.get(name)+ 1);
+		} else {
+			warenmenge.put(name, 1);
+		}
+	}
+	
+	public int get_quantity(String name) {
+		return warenmenge.getOrDefault(name, 0);
 	}
 }
--- 11_Aufgabe_Test/A2/ShoppingCart_Test.java
+++ 11_Aufgabe_Test/A2/ShoppingCart_Test.java
@@ -25,17 +25,18 @@ class ShoppingCart_Test {
 	void test_add_multiple_items_sums_total() {
 		ShoppingCart cart = new ShoppingCart();
 		cart.addItem("EinItem", 1);
-		cart.addItem("ZweiItem", 2);
+		cart.addItem("EinItem", 1);
 		cart.addItem("DreiItem", 3);
 		cart.addItem("VierItem", 4);
 		System.out.println(cart.warenkorb);
+		System.out.println(cart.warenmenge);
 		
-		assertEquals(10, cart.getTotal());
+		assertEquals(9, cart.getTotal());
 	}
 	
 	@Test
 	void test_get_quantity_of_item() {
 		ShoppingCart cart = new ShoppingCart();
-		assertEquals(0, cart.get_quantity());
+		assertEquals(0, cart.get_quantity("EinItem"));
 	}
 }
